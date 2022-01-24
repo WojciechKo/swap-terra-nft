e4942b77655c1c847125f0288ebcbdf9d3519acf
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2022-01-24 11:40:13 -0500

    Initial commit

---
+++ keys.terrain.js
@@ -0,0 +1,12 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+module.exports = {
+  bombay: {
+    mnemonic:
+      "vibrant balance observe limb detail jazz decline rose mask mirror wife uniform interest hedgehog rice spray miss coral supply net sketch expire gift velvet"
+  },
+  custom_tester_2: {
+    privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+  },
+};
